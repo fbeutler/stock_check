177672703e5acbc423cce3a22b36a2e0ba47cae9
Florian Beutler <florian.beutler@port.ac.uk>
2021-05-29 17:42:03 +0100

    latest

--- order.py
+++ order.py
@@ -4,7 +4,7 @@ import sys, os
 
 def main():
 
-	filename = 'existing_tags.txt'
+	filename = 'existing_tags2.txt'
 	# Check whether file exists... in which case we might attach
 	pbratios = []
 	tags = []
@@ -15,13 +15,8 @@ def main():
 			for line in f:
 				if line[0] != '#':
 					dummy = line.split()
-					if dummy[14] != 'None' and float(dummy[14]) > 0:
-						tags.append(dummy[0])
-						price.append(float(dummy[2]))
-						print("price[-1] = ", price[-1])
-						bvalue.append(float(dummy[14]))
-						print("bvalue[-1] = ", bvalue[-1])
-						pbratios.append(price[-1]/bvalue[-1])
+					tags.append(dummy[0])
+					pbratios.append(float(dummy[1]))
 
 	zipped_lists = zip(pbratios, tags)
 	sorted_zipped_lists = sorted(zipped_lists)
