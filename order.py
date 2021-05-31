798844b2fc8029cd1af71011403568ee78205bdd
Florian Beutler <florian.beutler@port.ac.uk>
2021-05-31 17:25:20 +0100

    latest

--- order.py
+++ order.py
@@ -52,20 +52,56 @@ def main():
 						sector_dict[int(dummy[-1])] = dummy[1]
 						industry_dict[int(dummy[-1])] = dummy[2]
 
+	filename = 'existing_tags_country.csv'
+	# Check whether file exists... in which case we might attach
+	country_dict = {}
+	if os.path.isfile(filename):
+		with open(filename, 'r') as f:
+			for line in f:
+				if line[0] != '#':
+					dummy = line.split(',')
+					country_dict[int(dummy[-1])] = dummy[1]
+
+	max_num = 200
+	max_dict = {'Financial Services': 0.20, 
+				'Real Estate': 0.1, 'Energy': 0.1,
+				'Argentina': 0.01, 'Greece': 0.01,
+				'Bermuda': 0.0, 'Panama': 0.0, 'Monaco': 0.0, 'Cayman Islands': 0.0}
+
 	counter = 0
 	zipped_lists = zip(pbratios, tags, global_ids)
 	sorted_zipped_lists = sorted(zipped_lists)
 	sectors = []
 	industries = []
+	countries = []
 	for pbratio, tag, global_id in sorted_zipped_lists:
-		if global_id in pe_dict and int(global_id) in sector_dict:
-			print(tag, pbratio, pe_dict[global_id], markets[int(global_id)], sector_dict[int(global_id)], industry_dict[int(global_id)])
-			sectors.append(sector_dict[int(global_id)])
-			industries.append(industry_dict[int(global_id)])
+		if global_id in pe_dict and int(global_id) in sector_dict and int(global_id) in country_dict:
+			if ((sector_dict[int(global_id)] in max_dict and 
+				sectors.count(sector_dict[int(global_id)]) < max_num*max_dict[sector_dict[int(global_id)]]) or
+				sector_dict[int(global_id)] not in max_dict):
 
-			counter += 1
-			if counter == 100:
-				break
+				if ((country_dict[int(global_id)] in max_dict and 
+					countries.count(country_dict[int(global_id)]) < max_num*max_dict[country_dict[int(global_id)]]) or
+					country_dict[int(global_id)] not in max_dict):
+					print(tag, pbratio, pe_dict[global_id], markets[int(global_id)], sector_dict[int(global_id)], industry_dict[int(global_id)], country_dict[int(global_id)])
+					sectors.append(sector_dict[int(global_id)])
+					industries.append(industry_dict[int(global_id)])
+					countries.append(country_dict[int(global_id)])
+
+					counter += 1
+					if counter == max_num:
+						break
+
+	unique_countries = list(set(countries))
+	num_countries = []
+	for el in unique_countries:
+		num_countries.append(countries.count(el))
+
+	print("unique_countries = ", unique_countries)
+	print("num_countries = ", num_countries)
+
+	plt.pie(num_countries, labels=unique_countries, autopct='%1.2f%%')
+	plt.show()
 
 	unique_industries = list(set(industries))
 	num_industries = []
